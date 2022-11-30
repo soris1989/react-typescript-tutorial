0e820010f8cf0c5dc958d9afcf6ccdba3681a0f1
DESKTOP-SK1K6UJ\ori <ori.sarah.sa@gmail.com>
2022-11-30 23:06:02 +0200

    feat: finish tutorial 10

--- src/10_TableMock/components/MemberTable.tsx
+++ src/10_TableMock/components/MemberTable.tsx
@@ -1,5 +1,25 @@
 import { useEffect } from "react";
 import { useMemberCollection } from "../hooks/useMemberCollection";
+import MemberEntity from "../model/members";
+
+const MemberRow = ({ member }: { member: MemberEntity }) => {
+    const { id, login, avatar_url } = member;
+    return (
+        <>
+            <tr>
+                <td>
+                    <img src={avatar_url} style={{ maxWidth: "10rem" }} />
+                </td>
+                <td>
+                    <span>{id}</span>
+                </td>
+                <td>
+                    <span>{login}</span>
+                </td>
+            </tr>
+        </>
+    );
+};
 
 export const MemberTable = (props: any) => {
     const { memberCollection, loadMemberCollection } = useMemberCollection();
@@ -10,9 +30,20 @@ export const MemberTable = (props: any) => {
 
     return (
         <>
-            {memberCollection.map((member) => (
-                <h1 key={member.id}>{member.login}</h1>
-            ))}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Id</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {memberCollection.map((member) => (
+                        <MemberRow key={member.id} member={member} />
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 };
