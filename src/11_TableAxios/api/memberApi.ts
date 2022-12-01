b62b81f79c03f31d40a3c6d78d5b1296b500b3a0
DESKTOP-SK1K6UJ\ori <ori.sarah.sa@gmail.com>
2022-12-01 22:05:37 +0200

    feat: integrate github api

--- src/11_TableAxios/api/memberApi.ts
+++ src/11_TableAxios/api/memberApi.ts
@@ -5,22 +5,6 @@ const gitHubURL = "https://api.github.com";
 const gitHubMembersUrl = `${gitHubURL}/orgs/lemoncode/members`;
 
 export const getMembersCollection = async (): Promise<MemberEntity[]> => {
-    // return new Promise<MemberEntity[]>((resolve, reject) => {
-    //     setTimeout(() => {
-    //         resolve([
-    //             {
-    //                 id: 1457912,
-    //                 login: "brauliodiez",
-    //                 avatar_url: "https://avatars.githubusercontent.com/u/1457912?v=3",
-    //             },
-    //             {
-    //                 id: 4374977,
-    //                 login: "Nasdan",
-    //                 avatar_url: "https://avatars.githubusercontent.com/u/4374977?v=3",
-    //             },
-    //         ]);
-    //     }, 500);
-    // });
     try {
         const { data } = await axios.get<MemberEntity[]>(gitHubMembersUrl);
         return data;
